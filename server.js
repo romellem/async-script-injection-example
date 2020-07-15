ab47ba03e2fab65a14b72a29a42d91e605acf5f9
romellem <romellem.git@gmail.com>
2020-07-15 17:15:36 -0500

    Fleshing out idea on delayed response
    Not going to use this, don't need to bring event emitters into this.

--- server.js
+++ server.js
@@ -2,9 +2,32 @@ const express = require('express');
 const morgan = require('morgan');
 const compression = require('compression');
 
+const createEventEmitterCode = (event_type) => {
+	return `(function() {
+var event;
+if (window.CustomEvent && typeof window.CustomEvent === 'function') {
+	event = new CustomEvent('${event_type}', { detail: null });
+} else {
+	event = document.createEvent('CustomEvent');
+	event.initCustomEvent('${event_type}', true, true);
+}
+
+document.dispatchEvent(event);
+})();`;
+};
+
+
 const app = express();
 app.use(morgan('combined'));
 app.use(compression());
+
+app.get('/js/big.js', (req, res) => {
+	res.type('js');
+	setTimeout(() => {
+		res.send('Hello');
+	}, 100);
+});
+
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
